87fdf29d6bd255efe632419dec4ff56a32049452
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-02-21 11:51:12 +0800

    added date js file

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather() {
-  const [city, setCity] = useState("");
-  const [ready, setReady] = useState(false);
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState({ ready: false });
 
   function displayWeather(response) {
-    setReady(true);
     setWeather({
+      ready: true,
+      city: response.data.name,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
-  function handleSubmit(event) {}
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  if (ready) {
+  if (weather.ready) {
     return (
       <div className="Weather">
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="col-9">
             <input
               type="search"
               placeholder="Search for a city"
               className="form-control"
-              onChange={updateCity}
             />
           </div>
           <div className="col-3">
@@ -42,9 +35,9 @@ export default function Weather() {
           </div>
         </form>
 
-        <h1>Los Angeles</h1>
+        <h1>{weather.city}</h1>
         <p>
-          Wednesday 07:00
+          <FormattedDate date={weather.date} />
           <br />
           Sunny
         </p>
@@ -58,7 +51,7 @@ export default function Weather() {
           </div>
           <div className="col-6">
             <uL>
-              <li>Description: {Math.round(weather.description)}</li>
+              <li>Description: {weather.description}</li>
               <li>Humidty: {Math.round(weather.humidity)}%</li>
               <li>Wind: {Math.round(weather.wind)} km/h</li>
             </uL>
@@ -67,7 +60,7 @@ export default function Weather() {
       </div>
     );
   } else {
-    let apiKey = "5201594abea9f3e38b70e65b11a80c24";
+    const apiKey = "5201594abea9f3e38b70e65b11a80c24";
     let city = `Los Angeles`;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(displayWeather);
